f2f625ad9f135912d641e621a1b7c30b227d0f9a
FelixvL <digitaleservice@gmail.com>
2022-02-24 10:44:17 +0100

    AB

--- Demo.java
+++ Demo.java
@@ -1,5 +1,9 @@
-class Demo{       // AA
+class Demo{       // AB
 	public static void main(String args[]){
 		System.out.println("hoi");
+		go();
+	}
+	public void go(){
+		System.out.println("doei");
 	}
 }
