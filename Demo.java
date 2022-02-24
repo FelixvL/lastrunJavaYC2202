e19084c94262bf81a3101e4927b689f3f64154bd
FelixvL <digitaleservice@gmail.com>
2022-02-24 15:35:51 +0100

    BJ

--- Demo.java
+++ Demo.java
@@ -1,29 +1,17 @@
-class Demo{       // BI
+class Demo{       // BJ
 	public static void main(String[] args){
-		Maker m = new Maker();
-		(Fiets)m.gaan().voortgaan();
-
+		System.out.println("s");
+		R r = new R();
+		r.t();
+		System.out.println("e");
 	}
 }
 
 
-class Fiets extends Voertuig{
-	int a;
-	void voortgaan(){
-		System.out.println("in gaan fiets");
-	}
-	
-}
-
-class Maker{
-	Fiets defiets = new Fiets();
-	Voertuig gaan(){
-		System.out.println("in gaan");
-		return defiets;
+class R{
+	void t(){
+		System.out.println("t");
+		throw new RuntimeException();
 	}
 
-}
-
-class Voertuig{
-	
 }
