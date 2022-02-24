b967d68edbe14630251160e19952371d88d60a0f
FelixvL <digitaleservice@gmail.com>
2022-02-24 10:46:15 +0100

    AC

--- Demo.java
+++ Demo.java
@@ -1,9 +1,15 @@
-class Demo{       // AB
+class Demo{       // AC
+	int a = hoi();
 	public static void main(String args[]){
+		new Demo().go();
 		System.out.println("hoi");
-		go();
+		new Demo().go();
 	}
 	public void go(){
 		System.out.println("doei");
+	}
+	int hoi(){
+		System.out.println("oeps");
+		return 7;
 	}
 }
