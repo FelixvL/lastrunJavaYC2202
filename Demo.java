c5e636c73cb83dd11ed54a3e0d71ebd25bb8c586
FelixvL <digitaleservice@gmail.com>
2022-02-24 15:40:00 +0100

    BO

--- Demo.java
+++ Demo.java
@@ -1,15 +1,19 @@
-class Demo{       // BM
+class Demo{       // BO
 	public static void main(String[] args){
 		System.out.println("s");
 		R r = new R();
-		r.t();
+		try{
+			r.t();
+		}catch(Exception e){
+			System.out.println("f");
+		}
 		System.out.println("e");
 	}
 }
 
 
 class R{
-	void t(){
+	void t() throws Exception{
 		System.out.println("t");
 		throw new Exception();
 	}
