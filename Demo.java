edeab766448ed9077ff9d9ab48e651c400716a52
FelixvL <digitaleservice@gmail.com>
2022-02-24 11:08:31 +0100

    AI

--- Demo.java
+++ Demo.java
@@ -1,5 +1,21 @@
-class Demo{       // AH
-	static final public void main(String ebc...){
+class Demo{       // AI
+	static final public void main(String[] ebc){
 		System.out.println("great");
+		Boven b = new Onder();
+		b.hoi();
 	}
+}
+
+
+class Boven{
+	void hoi(){
+		System.out.println("ik ben boven");
+	}
+}
+
+class Onder extends Boven{
+	void hoi(){
+		System.out.println("ik ben beneden");
+	}
+
 }
