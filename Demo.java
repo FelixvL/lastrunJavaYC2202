bd0a47886e1826d963fd71464549504baf3519fb
FelixvL <digitaleservice@gmail.com>
2022-02-24 15:23:59 +0100

    BF

--- Demo.java
+++ Demo.java
@@ -1,14 +1,16 @@
-class Demo{       // BE
+class Demo{       // BF
 	public static void main(String[] args){
 		Maker m = new Maker();
 		m.gaan().voortgaan();
-		System.out.println(m.gaan().a);
+		System.out.println(m.gaan().a++);
+		System.out.println(m.gaan().a++);
 
 	}
 }
 
 
 class Fiets extends Voertuig{
+	int a;
 	void voortgaan(){
 		System.out.println("in gaan fiets");
 	}
@@ -24,5 +26,5 @@ class Maker{
 }
 
 class Voertuig{
-	int a;
+	
 }
