a11e4d22ff1b873d556be8396149cb363961976b
FelixvL <digitaleservice@gmail.com>
2022-02-24 10:57:22 +0100

    AF

--- Demo.java
+++ Demo.java
@@ -1,20 +1,5 @@
-class Demo{       // AE
-	static int a = hoi();
-	int b = doei();
-	public static void main(String args[]){
-		System.out.println("hoi");
-		new Demo().go();
-		new Demo().go();
-	}
-	public void go(){
-		System.out.println("doei");
-	}
-	static int hoi(){
-		System.out.println("oeps");
-		return 7;
-	}
-	static int doei(){
-		System.out.println("anders");
-		return 7;
+class Demo{       // AF
+	final public void main(String... ebc){
+		System.out.println("great");
 	}
 }
