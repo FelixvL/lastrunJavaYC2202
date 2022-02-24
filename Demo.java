835ce302849d324117c14ff73f4bdd1512ebd89c
FelixvL <digitaleservice@gmail.com>
2022-02-24 15:05:28 +0100

    BB

--- Demo.java
+++ Demo.java
@@ -1,5 +1,20 @@
-class Demo{       // BA
+class Demo{       // BB
 	public static void main(String[] args){
 		System.out.println("vrijdag");
+		Fiets f = new Fiets();
+		f.mhhh = 3;
+		Fiets f2 = new Fiets();
+		System.out.println(f2.hmmm);
+		System.out.println(f2.mhhh);
 	}
+}
+
+
+class Fiets{
+	int hmmm;
+	static int mhhh;
+	Fiets(){
+		System.out.println("maandag");
+	}
+	
 }
