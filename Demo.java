60d2b936efdfc769a410bee6e3894a75a7dc77ab
FelixvL <digitaleservice@gmail.com>
2022-02-24 15:03:09 +0100

    BA

--- Demo.java
+++ Demo.java
@@ -1,23 +1,5 @@
-class Demo{       // AN
-	static final public void main(String[] ebc){
-		System.out.println("great");
-		hoezo().hoi();
+class Demo{       // BA
+	public static void main(String[] args){
+		System.out.println("vrijdag");
 	}
-	static Boven hoezo(){
-		return new Onder();
-	}
-}
-
-
-class Boven{
-	void hoi(){
-		System.out.println("ik ben boven");
-	}
-}
-
-class Onder extends Boven{
-	void hoi(){
-		System.out.println("ik ben beneden");
-	}
-
 }
