5e6b168da3957307a27ac3f1e5aa2ea28bcdd971
FelixvL <digitaleservice@gmail.com>
2022-02-25 11:48:11 +0100

    CD

--- Demo.java
+++ Demo.java
@@ -1,20 +1,27 @@
-class Demo{       // CC
+class Demo{       // CD
 	public static void main(String[] args){
 		System.out.println("Dreft");
 		Hoi h = new Hoi();
 		try{
 			h.go();
 			System.out.println("Milka");
-		}catch(Throwable e){   
+		}catch(Exception e){   
 			System.out.println("Witte Reus");
+		}finally{
+			System.out.println("Unox");
 		}
 		System.out.println("Ariel");
 	}
 }
 
 class Hoi{
+	boolean eum;
 	void go(){
-		throw new Error();  
+		if(eum){
+			throw new Error(); 
+		}else{
+			throw new Exception();
+		} 
 	}
 
 }
