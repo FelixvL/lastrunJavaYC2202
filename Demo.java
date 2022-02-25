9428a9628400898fbf9537c74f4c6fe2789bc73a
FelixvL <digitaleservice@gmail.com>
2022-02-25 15:08:33 +0100

    EP

--- Demo.java
+++ Demo.java
@@ -1,19 +1,30 @@
-class Demo{       // EM
+class Demo{       // EP
 	public static void main(String[] args){
 		Go ab = new Eend();
 		ab.wandelen();
-		
-
+		Land l = new Land();
+		l.hier().wandelen();
+		l.ho = new Eend();
 	}
 }
-
+class Land{
+	Go ho;
+	Go hier(){
+		System.out.println("in daar");
+		return new Eend();
+	}
+}
 interface Go{
-	int snelheid;
+	public static final int snelheid = 4;
 	void wandelen();
 }
-
 class Eend implements Go{
+	static int oei = abc();
+	static int abc(){
+		System.out.println("in hier");
+		return 7;
+	}
 	public void wandelen(){
-		System.out.println("Wandelen in Eend" + snelheid++);
+		System.out.println("Wandelen in Eend" + snelheid);
 	}
 }
