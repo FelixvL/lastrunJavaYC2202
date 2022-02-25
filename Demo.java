472ad3f3ad4e5b3be3e043751907303e77a573de
FelixvL <digitaleservice@gmail.com>
2022-02-25 14:56:54 +0100

    EI

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
-class Demo{       // EH
+class Demo{       // EI
 	public static void main(String[] args){
 			int[][] anders = {{5,6,7,8},{3},{88,55,33},null,{5,4,2}};
-			System.out.println(anders[3]);
+			System.out.println(anders[0][3]);
 		
 
 	}
