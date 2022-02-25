8effc165dfc406efc9a441373457ddf3fc762952
FelixvL <digitaleservice@gmail.com>
2022-02-25 11:43:05 +0100

    CB

--- Demo.java
+++ Demo.java
@@ -1,8 +1,13 @@
-class Demo{       // CA	
+class Demo{       // CB
 	public static void main(String[] args){
 		System.out.println("Dreft");
 		Hoi h = new Hoi();
-		h.go();
+		try{
+			h.go();
+			System.out.println("Milka");
+		}catch(Exception e){
+			System.out.println("Witte Reus");
+		}
 		System.out.println("Ariel");
 	}
 }
