437eaa54b1a211350fe164e378062c7d9548af8b
FelixvL <digitaleservice@gmail.com>
2022-02-25 15:17:19 +0100

    EW

--- Demo.java
+++ Demo.java
@@ -1,30 +1,24 @@
-class Demo{       // EQ
+class Demo{       // EW
 	public static void main(String[] args) throws Exception{
-		Go ab = new Eend();
-		ab.wandelen();
-		Land l = new Land();
-		l.hier().wandelen();
-		l.ho = new Eend();
+		new Makkelijk();
+		new Makkelijk();
 	}
 }
-class Land{
-	Go ho;
-	Go hier(){
-		System.out.println("in daar");
-		return new Eend();
+
+class Makkelijk{
+	static{
+		System.out.println("c");
 	}
-}
-interface Go{
-	public static final int snelheid = 4;
-	void wandelen() throws Exception;
-}
-class Eend implements Go{
-	static int oei = abc();
-	static int abc(){
-		System.out.println("in hier");
-		return 7;
+	{
+		System.out.println("b");
 	}
-	public void wandelen(){
-		System.out.println("Wandelen in Eend" + snelheid);
+	Makkelijk(){
+		System.out.println("a");
+	}
+	{
+		System.out.println("d");
+	}
+	static{
+		System.out.println("e");
 	}
 }
